22427f7669bbcdec110f59b6743407bfa088de1f
fshin3 <fshin3@gmail.com>
2021-11-29 11:49:50 +0900

    modify 24th

--- R/スクリプト/第24回.R
+++ R/スクリプト/第24回.R
@@ -1,36 +1,44 @@
+# 第23回作成データの読み込みと確認
+load("res_lm_catch.rda")
+head(res_lm_catch)
 
-# read data
-meteo_data <- read.csv("yokohama.csv")
-# edit data
-meteo_data <- meteo_data[-1,]
-plot(meteo_data$T)
-head(meteo_data)
-dim(meteo_data)
-names(meteo_data)
-class(meteo_data$year)
+# lm結果の残差プロット
+plot(res_lm_catch$residuals)
+hist(res_lm_catch$residuals)
 
-# plotしてチェック
-# plot(meteo_data$T)
-# 型がfactorになってしまっている場合は一旦characterに変換
-#for(i in 1:20){
-#  meteo_data[,i]<-as.character(meteo_data[,i])
-#}
-#class(meteo_data$year)
+# 正規乱数発生とqq plotを自力で
+stdnorm.smp <- rnorm(100,0,1)
+hist(stdnorm.smp)
+ordnum <- seq(1,100)
+prob <- (ordnum-0.5)/(max(ordnum))
+theo.smp<-qnorm(prob,0,1)
+ord.stdnorm.smp <- stdnorm.smp[order(stdnorm.smp,decreasing = F)]
+plot(theo.smp,ord.stdnorm.smp)
 
-# 解析に使う列のデータの型変換
-for(i in 1:20){
-  meteo_data[,i]<-as.numeric(meteo_data[,i])
-}
+# 正規乱数のqqplot
+qqnorm(stdnorm.smp)
 
-meteo_data_forlm <- subset(meteo_data,(meteo_data$year>2009))
-dim(meteo_data_forlm)
-meteo_data_forlm <- meteo_data_forlm[,4:20]
-names(meteo_data_forlm)
+# lm結果の残差に対してqqplot
+qqnorm(res_lm_catch$residuals)
 
-# lm
-res_lm_meteor <- lm(Tw0~.,data=meteo_data_forlm)
-summary(res_lm_meteor)
+# lmの結果オブジェクトをプロット
+plot(res_lm_catch)
 
-# library performance
+# glmの結果オブジェクトをプロット
+plot(res_glm_gala)
+
+# lm結果の残差に対してShapiro-Wikl test
+shapiro.test(res_lm_catch$residuals)
+
+# performance packageを利用
+install.packages("performance")
 library(performance)
-check_collinearity(res_lm_meteor)
+
+# check_normalityで正規性の確認
+no_check <- check_normality(res_lm_catch)
+
+# plotして確認
+# see packageが必要と出るのでインストール＆ライブラリ読み込みしてから
+# install.packages("see")
+# library(see)
+plot(no_check)

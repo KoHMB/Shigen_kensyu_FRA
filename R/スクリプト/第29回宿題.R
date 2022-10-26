aabdd75f236e8d07871d89cf2e71d5f24cc4bdf7
fshin3 <fshin3@gmail.com>
2022-10-26 17:21:03 +0900

    update R/30th and SR-03

--- R/スクリプト/第29回宿題.R
+++ R/スクリプト/第29回宿題.R
@@ -14,13 +14,17 @@ CI_param_glm_catch <- confint(res_glm_catch)
 
 pred_catch_data <- data.frame(vessel=catch_data$vessel,temp=catch_data$temp,obs=catch_data$catch,pred=res_glm_catch$fitted.values)
 
-pred_catch_data$lwr<-c(exp(CI_param_glm_catch[1,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v1")]),exp(CI_param_glm_catch[2,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v2")]),exp(CI_param_glm_catch[3,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v3")]))
-pred_catch_data$upr<-c(exp(CI_param_glm_catch[1,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v1")]),exp(CI_param_glm_catch[2,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v2")]),exp(CI_param_glm_catch[3,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v3")]))
+pred_catch_data$CI95_lwr<-c(exp(CI_param_glm_catch[1,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v1")]),exp(CI_param_glm_catch[2,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v2")]),exp(CI_param_glm_catch[3,1]+CI_param_glm_catch[4,1]*catch_data$temp[which(catch_data$vessel=="v3")]))
+pred_catch_data$CI95_upr<-c(exp(CI_param_glm_catch[1,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v1")]),exp(CI_param_glm_catch[2,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v2")]),exp(CI_param_glm_catch[3,2]+CI_param_glm_catch[4,2]*catch_data$temp[which(catch_data$vessel=="v3")]))
+
+
+pred_catch_data$PI95_lwr <- qpois(res_glm_catch$fitted.values,p = 0.025)
+pred_catch_data$PI95_upr <- qpois(res_glm_catch$fitted.values,p = 0.975)
 
 head(pred_catch_data)
 
 library(RColorBrewer)
-plot_data <- function(data){
+plot_data <- function(data,interval="CI"){
   par(oma=c(1,1,0,0))
   col <- brewer.pal(length(unique(data$vessel)),"Set1")
   i <- 1
@@ -31,10 +35,18 @@ plot_data <- function(data){
     plot(data_tmp$temp, data_tmp$obs, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="p", pch=21, cex=0.5)
     par(new=T)
     plot(data_tmp$temp, data_tmp$pred, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l")
-    par(new=T)
-    plot(data_tmp$temp, data_tmp$upr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
-    par(new=T)
-    plot(data_tmp$temp, data_tmp$lwr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
+    if(interval=="CI"){
+      par(new=T)
+      plot(data_tmp$temp, data_tmp$CI95_upr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
+      par(new=T)
+      plot(data_tmp$temp, data_tmp$CI95_lwr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
+    }else if(interval=="PI"){
+      par(new=T)
+      plot(data_tmp$temp, data_tmp$PI95_upr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
+      par(new=T)
+      plot(data_tmp$temp, data_tmp$PI95_lwr, xlim=c(min(data$temp),max(data$temp)), ylim=c(min(data$obs), max(data$obs)), col=col[i], xlab="", ylab="", type="l",lty ="dashed")
+      
+    }
     i <- i+1
   }
   mtext("Temperature", side=1, cex=1.2, outer=T)
@@ -43,3 +55,4 @@ plot_data <- function(data){
 
 plot_data(pred_catch_data)
 
+plot_data(pred_catch_data,interval = "PI")
--- R/スクリプト/第30回.R
+++ R/スクリプト/第30回.R
@@ -3,22 +3,16 @@
 catch_data <- read.csv("catch_data2.csv")
 
 # install glmmML
-install.packages(glmmML)
+install.packages("glmmML")
 library(glmmML)
 
 res_glmm_catch <- glmmML(catch~vessel+temp,family = poisson(link = "log"), data = catch_data, cluster = as.factor(area), method = "Laplace")
 summary(res_glmm_catch)
 
-#install.packages("lme4")
-library(lme4)
-res_glmm_catch2 <- glmer(catch~vessel+temp+(1|area),family = poisson(link = "log"), data = catch_data)
-summary(res_glmm_catch2)
-
 # compare aic
 res_glm_catch$aic   # glm; catch ~ vessel + temp
 res_glm_catch2$aic  # glm; catch ~ vessel + temp + area
 res_glmm_catch$aic  # glmm;catch ~ vessel + temp + random(area) (glmmML)
-res_glmm_catch2$aic # glmm;catch ~ vessel + temp + random(area) (glmer)
 
 # glmm結果から推定係数の取り出し
 #固定効果

acb93ca82e052ec347e2edb8bc7bfdd804233bb2
fshin3 <fshin3@gmail.com>
2022-11-29 14:04:10 +0900

    update 27th and 29th

--- R/スクリプト/第27回宿題.R
+++ R/スクリプト/第27回宿題.R
@@ -1,21 +1,15 @@
 # glm catch_data2
 # read catch data
-catch_data <- read.csv("catch_data2.csv")
-
+catch_data2 <- read.csv("catch_data2.csv")
+head(catch_data2)
 # glm; catch ~ vessel + temp + area
-res_glm_catch <- glm(catch~as.factor(vessel)+temp+as.factor(area)-1,family = poisson(link="log"),data=catch_data)
+res_glm_catch <- glm(catch~as.factor(vessel)+temp+as.factor(area)-1,family = poisson(link="log"),data=catch_data2)
 summary(res_glm_catch)
 res_glm_catch$aic
 
-library(MuMIn)
-options(na.action="na.fail")
-dredge_glm_catch<-dredge(res_glm_catch,rank=AIC)
-res_dredge_glm_catch<-get.models(dredge_glm_catch,subset=1)
-summary(res_dredge_glm_catch$'8')
-
 # count data# by each area
-barplot(table(catch_data$area))
+barplot(table(catch_data2$area))
 
 # glm; catch ~ vessel + temp
-res_glm_catch2 <- glm(catch~as.factor(vessel)+temp-1,family = poisson(link="log"),data=catch_data)
+res_glm_catch2 <- glm(catch~as.factor(vessel)+temp-1,family = poisson(link="log"),data=catch_data2)
 res_glm_catch2$aic
--- R/スクリプト/第29回.R
+++ R/スクリプト/第29回.R
@@ -2,6 +2,29 @@
 catch_data2 <- read.csv("catch_data2.csv")
 barplot(table(catch_data2$area))
 
+# areaを含んだglm
+res_glm_catch <- glm(catch~as.factor(vessel)+temp+as.factor(area)-1,family = poisson(link=log),data = catch_data2)
+
+pred_catch_data <- expand.grid(vessel=unique(catch_data2$vessel),temp=seq(min(catch_data2$temp),max(catch_data2$temp),length=20),area=unique(catch_data2$area))
+pred_catch_data$pred <- predict(res_glm_catch,newdata = pred_catch_data,type = "response") 
+
+# vessel1について温度を横軸にcatchを縦軸にプロット
+pred_catch_datav1<-pred_catch_data[which(pred_catch_data$vessel=="v1"),]
+# plotの軸のデータ範囲
+yrange=c(min(pred_catch_datav1$pred),max(pred_catch_datav1$pred))
+xrange=c(min(pred_catch_datav1$temp),max(pred_catch_datav1$temp))
+# 目的変数のプロット
+plot(catch_data2$temp[which(catch_data2$vessel=="v1",)],catch_data2$catch[which(catch_data2$vessel=="v1",)],xlab="temp",ylab="catch",ylim=yrange,xlim=xrange)
+
+library(RColorBrewer)
+colPal3 <- colorRampPalette(brewer.pal(11, "Spectral"))
+col= colPal3(length(unique(catch_data2$area)))
+# 約60本の線を入れるので時間がかかります
+for(i in 1:(length(unique(catch_data2$area))-1)){
+  par(new=T)
+  plot(pred_catch_datav1$temp[which(pred_catch_datav1$area==i)],pred_catch_datav1$pred[which(pred_catch_datav1$area==i)],type="l",col=col[i],lwd=0.5,xlab="",ylab="",ylim=yrange,xlim=xrange)
+}
+
 # install glmmML
 # install.packages("glmmML")
 library(glmmML)

708e55b9a1b62b23e19d80f2a92ee35b0aed3797
Shin FUKUI <fshin3@gmail.com>
2024-10-01 19:35:44 +0900

    update 39th script

--- R/スクリプト/第39回.R
+++ R/スクリプト/第39回.R
@@ -5,7 +5,7 @@ library(vegan)
 # データ読み込み
 FishColEnv <- read.csv("FishColEnv.csv")
 names(FishColEnv)
-
+head(FishColEnv)
 # 体色についてのデータのデータフレーム（目的変数）
 FishColTrait <- FishColEnv[,c(2:7)]
 # 生息環境についてのデータのデータフレーム（説明変数）
@@ -19,12 +19,22 @@ plot(FishHabitatEnv)
 # decorana関数で説明変数の傾向チェック
 res_dca<-decorana(FishHabitatEnv)
 # decorana関数は正の値にのみ有効なので、データの中の負の値を処理する
-# 今回は水温-1℃のみで、0にしてしまう。絶対温度をとっても良い（+275℃でK）
+# 今回は水温-1℃のみで、0にしてしまう。絶対温度をとっても良い（+273.15℃でK）
 FishHabitatEnv$minTemp[1]<-0
 res_dca<-decorana(FishHabitatEnv)
-plot(res_dca)
 
 # RDA解析（ただし、変数のスケールをとる）
-res_rda <- rda(Y=FishColTrait,X=FishHabitatEnv,scale=T)
+res_rda <- rda(FishColTrait~.,data=FishHabitatEnv,scale=T)
 summary(res_rda)
+res_rda$tot.chi
 plot(res_rda)
+row.names(FishColTrait)<-FishColEnv$Species
+res_rda <- rda(FishColTrait~.,data=FishHabitatEnv,scale=T)
+plot(res_rda)
+par(family="Osaka")
+
+FishColTraitDorsal<-data.frame(FishColTrait$DorsalRed,FishColTrait$DorsalGreen,FishColTrait$DorsalBlue)
+row.names(FishColTraitDorsal)<-FishColEnv$Species
+res_rda2 <- rda(FishColTraitDorsal~.,data=FishHabitatEnv,scale=T)
+summary(res_rda2)
+plot(res_rda2)
